590050b072ef209969e34f2f601ab9c764a34e2b
Philip Szalwinski <pszalwinski@gmail.com>
2013-09-11 15:03:06 -0500

    Computer player can score board; working on minimax

--- src/computer.erl
+++ src/computer.erl
@@ -3,3 +3,21 @@
 
 take_turn(Board, Piece) ->
   lalalalala.
+
+score_board(Board, Piece) ->
+  Winner = game_rules:winner(Board),
+  if
+    Winner =:= false -> 0;
+    Winner =:= Piece -> 1;
+    Winner =/= Piece -> -1
+  end.
+
+minimax(Board, Piece, Depth) ->
+  GameStatus = game_rules:game_over(Board),
+  Score = 0,
+  BestScore = -1000000,
+  case GameStatus of
+    true  -> score_board(Board, Piece)/Depth;
+    false -> true
+  end.
+  
--- test/computer_test.erl
+++ test/computer_test.erl
@@ -1,2 +1,34 @@
 -module(computer_test).
 -include_lib("eunit/include/eunit.hrl").
+
+score_board_test() ->
+  [{ "provides a 1 for win, -1 for loss, and 0 for tie",
+      ?assertEqual(
+        1, computer:score_board([x,x,x,4,5,6,7,8,9], x))
+    },
+    
+    { "provides a 1 for win, -1 for loss, and 0 for tie",
+      ?assertEqual(
+        -1, computer:score_board([x,x,x,4,5,6,7,8,9], o))
+    },
+    
+    { "provides a 1 for win, -1 for loss, and 0 for tie",
+      ?assertEqual(
+        0, computer:score_board([x,o,x,x,o,x,o,x,o], o))
+    }].
+
+minimax_test() ->
+    [{ "provides a 1 for win, -1 for loss, and 0 for tie",
+        ?assertEqual(
+          1.0, computer:minimax([x,x,x,4,5,6,7,8,9], x, 1))
+      },
+      
+      { "provides a 1 for win, -1 for loss, and 0 for tie",
+        ?assertEqual(
+          -1.0, computer:minimax([x,x,x,4,5,6,7,8,9], o, 1))
+      },
+      
+      { "provides a 1 for win, -1 for loss, and 0 for tie",
+        ?assertEqual(
+          0.0, computer:minimax([x,o,x,x,o,x,o,x,o], o, 1))
+      }].
