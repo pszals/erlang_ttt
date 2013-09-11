0b2a789e1878cc4f4432cf036518f35fd071a4c7
Philip Szalwinski <pszalwinski@gmail.com>
2013-09-10 22:24:19 -0500

    Human gets move from console; validates move; returns modified board

--- src/human.erl
+++ src/human.erl
@@ -2,4 +2,15 @@
 -compile(export_all).
 
 take_turn(Board, Piece) ->
-  lalalal.
+  Input = new_console_io:input(),
+  Formatted = new_console_io:format_input(Input),
+  case  validate(Formatted, Board) of
+    true -> game_rules:store_move(Formatted, Piece, Board);
+    false -> console_io:invalid_input(),
+      take_turn(Board, Piece)
+  end.
+
+
+
+validate(Move, Board) ->
+  board:square_open(Move, Board) andalso console_io:validate(Move).
--- test/human_test.erl
+++ test/human_test.erl
@@ -1,14 +1,18 @@
 -module(human_test).
 -include_lib("eunit/include/eunit.hrl").
-%setup() ->
-%  meck:new(new_console_io).
-%
-%cleanup() ->
-%  meck:unload(io).
-%
-%take_turn_test() ->
-%  [{ "takes a turn",
-%      setup(),
-%      meck:expect(new_console_io, input, 
-%      cleanup()
-%    }].
+
+setup() ->
+  meck:new(new_console_io).
+
+cleanup() ->
+  meck:unload(new_console_io).
+
+take_turn_test() ->
+  [{ "takes a turn",
+      setup(),
+      meck:expect(new_console_io, input, fun() -> "1" end),
+      meck:expect(new_console_io, format_input, fun("1") -> 1 end),
+      ?assertEqual(
+        [x], human:take_turn([1], x)),
+      cleanup()
+    }].
