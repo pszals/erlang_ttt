c7655a8494f082c3499e4dde1d63b7db2bacd369
Philip Szalwinski <pszalwinski@gmail.com>
2013-09-13 11:02:26 -0500

    AI is partially working

--- src/computer.erl
+++ src/computer.erl
@@ -2,52 +2,49 @@
 -compile(export_all).
 
 take_turn(Board, Piece) ->
-  lalala.
-%  lists:max.
+  Square = best_square(Board, Piece),
+  game_rules:store_move(Square, Piece, Board).
+
+best_square(Board, Piece) ->
+  OpenSquares = game_rules:open_squares(Board),
+  ScoreReport = lists:map(space_report(Board, Piece), OpenSquares),
+  {Score, Square} = lists:max(ScoreReport),
+  Square.
 
 score_board(Board, Piece) ->
   Winner = game_rules:winner(Board),
   if
     Winner =:= false -> 0;
-    Winner =:= Piece -> 1;
-    Winner =/= Piece -> -1
+    Winner =:= Piece -> 1.0;
+    Winner =/= Piece -> -1.0
   end.
 
-%                       mini-max
-%                     {space, amount}
-%                        summer
-%                        (every possible move)
-%                        [1]
-%                         2
-%                        / /  
-%                     (0)D /\(2)
-%                       0 W L
-%                         1 1 
-% entry
-minimax(Board, Piece) ->
+score(Board, Piece) ->
   OpenSquares = game_rules:open_squares(Board),
-  % Map minimax over these open squares
-  % lists:map(next_level_score(Board, Piece, Depth, OpenSquares, Score), OpenSquares)
-  % Returning [{1, 10}, {space, score}] return best scored space
- % pass in space here 
-  minimax(Board, Piece, 1, OpenSquares, 0). 
+  score(Board, Piece, 1, OpenSquares, 0). 
 
-minimax(Board, Piece, Depth, OpenSquares, Score) ->
+score(Board, Piece, Depth, OpenSquares, Score) ->
   GameStatus = game_rules:game_over(Board),
-  BestScore = -1000000,
   case GameStatus of
     true  -> score_board(Board, Piece)/Depth;
     false -> 
-      lists:map(next_level_score(Board, Piece, Depth, OpenSquares, Score), OpenSquares)
+      Scores = lists:map(next_level_score(Board, Piece, Depth + 1, OpenSquares, Score), OpenSquares),
+    lists:sum(Scores)
+  end.
+
+space_report(Board, Piece) ->
+  fun(Square) ->
+    NewBoard = game_rules:store_move(Square, Piece, Board),
+    {score(NewBoard, Piece), Square}
   end.
 
 next_level_score(Board, Piece, Depth, OpenSquares, Score) ->
   fun(Square) ->
-      NewDepth = Depth + 1,
-      NewBoard = game_rules:store_move(Square, Piece, Board),
-      NewPiece = switch_piece(Piece),
-      NewOpenSquares = game_rules:open_squares(NewBoard),
-      minimax(NewBoard, NewPiece, NewDepth, NewOpenSquares, Score)
+    NewDepth = Depth + 1,
+    NewBoard = game_rules:store_move(Square, Piece, Board),
+    NewPiece = switch_piece(Piece),
+    NewOpenSquares = game_rules:open_squares(NewBoard),
+    score(NewBoard, NewPiece, NewDepth, NewOpenSquares, Score)
   end.
 
 switch_piece(Piece) ->
--- test/computer_test.erl
+++ test/computer_test.erl
@@ -4,34 +4,18 @@
 score_board_test() ->
   [{ "provides a 1 for win, -1 for loss, and 0 for tie",
       ?assertEqual(
-        1, computer:score_board([x,x,x,4,5,6,7,8,9], x))
+        1.0, computer:score_board([x,x,x,4,5,6,7,8,9], x))
     },
     
     { "provides a 1 for win, -1 for loss, and 0 for tie",
       ?assertEqual(
-        -1, computer:score_board([x,x,x,4,5,6,7,8,9], o))
+        -1.0, computer:score_board([x,x,x,4,5,6,7,8,9], o))
     },
     
     { "provides a 1 for win, -1 for loss, and 0 for tie",
       ?assertEqual(
         0, computer:score_board([x,o,x,x,o,x,o,x,o], o))
     }].
-
-minimax_test() ->
-    [{ "provides a 1 for win, -1 for loss, and 0 for tie",
-        ?assertEqual(
-          1.0, computer:minimax([x,x,x,4,5,6,7,8,9], x))
-      },
-      
-      { "provides a 1 for win, -1 for loss, and 0 for tie",
-        ?assertEqual(
-          -1.0, computer:minimax([x,x,x,4,5,6,7,8,9], o))
-      },
-      
-      { "provides a 1 for win, -1 for loss, and 0 for tie",
-        ?assertEqual(
-          0.0, computer:minimax([x,o,x,x,o,x,o,x,o], o))
-      }].
 
 switch_piece_test() ->
   [{ "switches piece",
@@ -42,4 +26,45 @@ switch_piece_test() ->
     { "switches piece",
       ?assertEqual(
         x, computer:switch_piece(o))
+    }].
+
+best_square_test() ->
+  [{ "correctly implements minimax to make a move",
+      ?assertEqual(
+        4, computer:best_square(
+          [
+            1,x,x,
+            4,o,o,
+            7,8,9
+          ], o))
+    },
+    
+    { "correctly implements minimax to make a move",
+      ?assertEqual(
+        8, computer:best_square(
+          [
+            x,2,3,
+            4,o,6,
+            7,8,x
+          ], o))
+    },
+    
+    { "correctly implements minimax to make a move",
+      ?assertEqual(
+        3, computer:best_square(
+          [
+            x,x,3,
+            4,o,6,
+            7,o,x
+          ], x))
+    },
+    
+    { "correctly implements minimax to make a move",
+      ?assertEqual(
+        3, computer:best_square(
+          [
+            x,x,3,
+            4,5,6,
+            7,8,o
+          ], o))
     }].
